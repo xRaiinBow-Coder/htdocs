a97fb4da4aea6237c4875111c217753d808f29cd
Kieran <Kierancoder@gmail.com>
2024-08-06 22:46:40 +0100

    Start of Something New

---
+++ Portfolio/Main.php
@@ -0,0 +1,23 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <link href="style.css" rel="stylesheet">
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Main Page</title>
+</head>
+<body>
+    <div class="Nav">
+        <ul>
+            <li a href="main.php">Home</a></li>
+            <li a href="">About</a></li>
+            <li a href="">Portfolio</a></li>
+            <li a href="">Contacts</a></li>
+        </ul>
+    </div>
+    <div class="Beginning">
+        <h2>WELCOME TO THE CREATION</h2>
+    </div>
+    
+</body>
+</html>
