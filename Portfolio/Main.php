7d2ef4da7780369af18406f92ebd0958bd0613b5
Kieran <Kierancoder@gmail.com>
2024-08-08 23:33:20 +0100

    Css is my nightmare which i love to fuel

--- Portfolio/Main.php
+++ Portfolio/Main.php
@@ -3,20 +3,22 @@
 <head>
     <link rel="stylesheet" href="style.css">
     <link rel="stylesheet" href="main.css">
+    <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/4.7.0/css/font-awesome.min.css">
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>Main Page</title>
-
+    
     <div class="Nav">
         <ul>
-            <li a href="main.php">Home</a></li>
-            <li a href="">About</a></li>
-            <li a href="">Portfolio</a></li>
-            <li a href="">Contacts</a></li>
+            <li><a href="main.php">Home</a></li>
+            <li><a href="">Aboute</a></li>
+            <li><a href="">Portfolio</a></li>
+            <li><a href="">Contacts</a></li>
         </ul>
     </div>
 </head>
 <body>
+
     <div class="title">
         <h1>About me</h1>
         <p class="About-me">
@@ -27,7 +29,17 @@
     </div>
     <div class="me">
         <img src="me.jpg" alt="image" height="350px" width="350px">
-        <img src="git.png" alt="right" height="50px" width="50px">
+        <img src="code.jpg" alt="image">
+        <img src="PlanetE.png" alt="image" height="700px" width="700px">
+
+    </div>
+
+    <div class="Button">
+        <ul>
+            <li><a href="#"><i class="fa fa-facebook"></i></a></li>
+            <li><a href="#"><i class="fa fa-github"></i></a></li>
+            <li><a href="#"><i class="fa fa-instagram"></i></a></li>
+        </ul>
     </div>
 
 </body>
