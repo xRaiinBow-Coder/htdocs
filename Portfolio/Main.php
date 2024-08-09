223a66a4ca4670314be6c6386c1b79ed1daef3ee
Kieran <Kierancoder@gmail.com>
2024-08-09 23:51:27 +0100

    Trying animation, also changed the layout

--- Portfolio/Main.php
+++ Portfolio/Main.php
@@ -16,9 +16,9 @@
         </ul>
         <div class="Button">
             <ul>
-                <li><a href="#"><i class="fa fa-facebook"></i></a></li>
-                <li><a href="#"><i class="fa fa-github"></i></a></li>
-                <li><a href="#"><i class="fa fa-instagram"></i></a></li>
+                <li><a href="https://www.facebook.com/" target="_blank"><i class="fa fa-facebook" name="Facebook"></i></a></li>
+                <li><a href="https://github.com/xRaiinBow-Coder" target="_blank"><i class="fa fa-github" name="Github" type="submit"></i></a></li>
+                <li><a href="https://www.instagram.com/" target="_blank"><i class="fa fa-instagram" name="Instagram" ></i></a></li>
             </ul>
     </div>
     </div>
@@ -28,16 +28,25 @@
 <body>
 
     <div class="title">
-        <h1>Kieran Jefferson</h1>
+        <h1 class="Heading">Programmer</h1>
         <p class="About">
         fdshfhsjdkhfjksdjkfhkjsdhfjkhsdjkfhkjsdhkjfhsdkjhfkjsdhkjfhsdkjhk<br>
         fsdhfksdfjksdhfkjsdjkfhkjsdhkjfhskjdhfkjsdhkjfhkjsdfkjsdkjfhkjsdh<br>
         fshdfhjksdjkfhsdkjhfkjsdkjfhsdjkhfjksdhkjfhsdjkhfkjsdhfkjhsdkjhfk<br>
         </p>
+
+        <div class="title2">
+            <h1 class="Heading2">Coder</h1>
+            <p class="about2">
+            fdshfhsjdkhfjksdjkfhkjsdhfjkhsdjkfhkjsdhkjfhsdkjhfkjsdhkjfhsdkjhk<br>
+            fsdhfksdfjksdhfkjsdjkfhkjsdhkjfhskjdhfkjsdhkjfhkjsdfkjsdkjfhkjsdh<br>
+            fshdfhjksdjkfhsdkjhfkjsdkjfhsdjkhfjksdhkjfhsdjkhfkjsdhfkjhsdkjhfk<br>   
+            </p>
+        </div>
     </div>
     <div class="me">
         <img src="me.jpg" alt="image" height="350px" width="350px">
-        <img src="code.jpg" alt="image">
+        <img src="coder.png" alt="image">
         <img src="PlanetE.png" alt="image" height="750px" width="750px">
 
     </div>
@@ -53,3 +62,5 @@
 
 </body>
 </html>
+<?php
+?>
